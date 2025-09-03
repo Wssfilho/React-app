138322c88bb69068b3313f0f9de188d69bee844a
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-09-03 20:19:41 -0300

    feat: implementar página de login e adicionar manipulação de envio de formulário

---
+++ src/app/(private)/tasks/page.tsx
@@ -0,0 +1,7 @@
+export default function Tasks() {
+    return (
+        <div>
+            <h1>Tasks Page - Private</h1>
+        </div>
+    )
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import Button from '../components/Button'
 import styles from './page.module.css'
+import { FormEvent } from 'react';
 export default function Login() {
+    const router = useRouter();
 
+    function handleFormSubmit(event : FormEvent<HTMLFormElement>) {
+        event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+        router.push('/tasks');
+    }
 
     return (
         <div className={styles.container}>
@@ -12,12 +20,12 @@ export default function Login() {
                     </h1>
                     <p>Insira as informações de login:</p>
                 </header>
-                <form className={styles.form}>
+                <form className={styles.form}
+                    onSubmit={(e) => handleFormSubmit(e)}>
                     <input type="text" className={styles.input} placeholder='Usuário' />
                     <input type="text" className={styles.input} placeholder='Senha' />
                     <Button size='lg' variant='primary' type='submit'>
                         LOGIN
-                        
                     </Button>
                 </form>
             </div>
